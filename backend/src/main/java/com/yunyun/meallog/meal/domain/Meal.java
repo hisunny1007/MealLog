22c35db8578f8f96186e236901547e0caa699909
hisunny1007 <hisunny1007@gmail.com>
2025-12-05 17:31:40 +0900

    feat: meal 엔티티구현

---
+++ backend/src/main/java/com/yunyun/meallog/meal/domain/Meal.java
@@ -0,0 +1,9 @@
+package com.yunyun.meallog.meal.domain;
+
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@NoArgsConstructor
+@
+public class Meal {
+}
