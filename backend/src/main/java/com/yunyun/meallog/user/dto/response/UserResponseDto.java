d69a6276a5986c2c02fc373d62252a2c557beb00
YSH <sanghyun.yoon128@gmail.com>
2025-12-17 17:32:37 +0900

    fix: users DB 재생성으로 userId 자료형 수정, LoginView, SignUpView 디자인 수정, MyPageView 생성

--- backend/src/main/java/com/yunyun/meallog/user/dto/response/UserResponseDto.java
+++ backend/src/main/java/com/yunyun/meallog/user/dto/response/UserResponseDto.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponseDto {
-    private Long id;
+    private Integer id;
     private String email;
     private String nickname;
     private Integer age;
