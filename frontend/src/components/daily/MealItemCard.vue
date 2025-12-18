48a0868184e2a7e6405f357ccf118ba31bd915ad
hisunny1007 <hisunny1007@gmail.com>
2025-12-19 01:38:31 +0900

    feat: 이미지 등록 조회 기능 구현

--- frontend/src/components/daily/MealItemCard.vue
+++ frontend/src/components/daily/MealItemCard.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="card meal-card">
-    <img :src="meal.imageUrl" class="meal-img" />
-    <div class="card-body">
-      <h5>{{ meal.foodName }}</h5>
+  <div class="meal-card-layout border rounded-4 p-3">
+    <section class="meal-left">
+      <img
+        v-if="meal.imageUrl"
+        :src="getImageUrl(meal.imageUrl)"
+        class="meal-image meal-layout mb-3"
+        alt="식단 이미지"
+      />
+    </section>
+
+    <h5 class="fw-bold">{{ meal.foodName }}</h5>
+
+    <div class="info-card-layout text-muted small">
       <p>{{ meal.calories }} kcal</p>
       <p>{{ meal.carbs }} g</p>
       <p>{{ meal.protein }} g</p>
       <p>{{ meal.fat }} g</p>
+      <p class="mt-2">{{ meal.memo }}</p>
     </div>
+
   </div>
 </template>
 
@@ -15,6 +26,27 @@
 defineProps({
   meal: Object,
 })
+
+const getImageUrl = (filename) => {
+  console.log('사진이름', filename)
+
+  if (!filename) return '/default-meal.png' // 기본 이미지
+  return `http://localhost:8080/api/v1/uploads/${filename}`
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.meal-card-layout {
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  gap: 24px;
+}
+
+.meal-image {
+  /* width: 100%;
+  height: 160px; */
+  object-fit: cover;
+  border-radius: 12px;
+}
+
+</style>
