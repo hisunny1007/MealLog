7a245e55c59b2935ef6ea341c1c582a6edcad5fb
hisunny1007 <hisunny1007@gmail.com>
2025-12-19 18:00:48 +0900

    fix: authStore 사용

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,20 +1,19 @@
+import { useAuthStore } from '@/stores/authStore'
 import axios from 'axios'
 
-// vite.config.js 프록시 추가
 const api = axios.create({
   baseURL: '/api/v1',
 })
 
-// 요청 인터셉터 추가 (로그인 이후 요청)
+// 요청 인터셉터
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('accessToken')
-    console.log('🔐 axios interceptor token =', token)
+    const authStore = useAuthStore()
+    const token = authStore.token
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     } else {
-      console.error('❌ 인증 실패 (JWT 없음 또는 만료)')
     }
 
     return config
@@ -22,14 +21,12 @@ api.interceptors.request.use(
   (error) => Promise.reject(error),
 )
 
-/**
- * 응답 인터셉터 (401 확인용)
- */
+// 응답 인터셉터 (401 확인용)
 api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      console.error('❌ 인증 실패 (JWT 없음 또는 만료)')
+      console.error('인증 실패')
     }
     return Promise.reject(error)
   },
