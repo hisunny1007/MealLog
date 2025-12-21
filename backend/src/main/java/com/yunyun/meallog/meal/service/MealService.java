af24d72ef669a25e46073cf251e453a8f5888469
hisunny1007 <hisunny1007@gmail.com>
2025-12-22 02:04:59 +0900

    feat: 식단 등록 시 최신 포인트 적립 기능 구현
    -MealResponseDto, MealCreateResponseDto 분리

--- backend/src/main/java/com/yunyun/meallog/meal/service/MealService.java
+++ backend/src/main/java/com/yunyun/meallog/meal/service/MealService.java
@@ -2,6 +2,7 @@ package com.yunyun.meallog.meal.service;
 
 import com.yunyun.meallog.meal.dto.request.MealRequestDto;
 import com.yunyun.meallog.meal.dto.response.MealCalendarSummaryResponseDto;
+import com.yunyun.meallog.meal.dto.response.MealCreateResponseDto;
 import com.yunyun.meallog.meal.dto.response.MealResponseDto;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,7 +14,7 @@ public interface MealService {
 
     //service는 dto주고받는 레이어라 엔티티 대신 dto 반환이 더 바람직함
 
-    MealResponseDto createMeal(Long userId, MealRequestDto requestDto, MultipartFile image);
+    MealCreateResponseDto createMeal(Long userId, MealRequestDto requestDto, MultipartFile image);
 
     MealResponseDto getMeal(Long userId, Long mealId);
 
